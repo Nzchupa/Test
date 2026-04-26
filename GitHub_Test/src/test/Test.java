2a44b7e11271618426fa52f50bb8b81833b0498d
Nzchupa <nazar16052005@gmail.com>
2026-04-26 17:03:05 +0200

    ja

--- GitHub_Test/src/test/Test.java
+++ GitHub_Test/src/test/Test.java
@@ -9,6 +9,7 @@ public class Test {
 System.out.println("Hallo Wiam");
 System.out.print("Funktionirt alles?");
 System.out.println(" Wenn nein, gib mir bescheid");
+System.out.println("Wenn ja, dann werde ich für unsere Project git erstellen");
 	}
 
 }
