9aef61e40b4b95154bc8c14db430cd895fff1dea
Nzchupa <nazar16052005@gmail.com>
2026-04-26 16:21:45 +0200

    neu änderungen

--- GitHub_Test/src/test/Test.java
+++ GitHub_Test/src/test/Test.java
@@ -1,10 +1,13 @@
 package test;
 
+
+
 public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+System.out.println("Hallo Wiam");
+System.out.print("Funktionirt alles?");
 	}
 
 }
